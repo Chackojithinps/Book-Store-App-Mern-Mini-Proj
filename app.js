f05bc7483f710674e56eeb61fba471f6a9b5f686
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-14 01:57:23 +0530

    router part  starting

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express= require('express')
 const mongoose = require('mongoose')
-
+const router = require('./routes/book-router')
 const app= express()
 
 //Middlewares
-
-app.use('/',(req,res,next)=>{
-  res.send("This is our starting application")
-})
+app.use("/books",router)
 
 mongoose.connect("mongodb+srv://admin:5hFg7rXsULjnAh6a@cluster0.ni6vsgm.mongodb.net/bookStore?retryWrites=true&w=majority").then(()=>{
   console.log("connected to db")
--- routes/book-router.js
+++ routes/book-router.js
@@ -1,6 +1,18 @@
 const express= require('express')
 const router = express.Router()
-
-router.get('/',(req,res,next)=>{
-    
+const Book = require('../model/bookModel')
+router.get('/', async (req,res,next)=>{
+   let books;
+   try {
+      books = await Book.find();
+   } catch (error) {
+     console.log(error.message)
+   }
+   if(!books){
+         return res.status(404).json({message:"No products found"})
+   }else{
+       return res.status(200).json({books})
+   }
 })
+
+module.exports = router
