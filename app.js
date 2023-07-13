89d42627207b99e0b83ad5c7d200a87989343e4c
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-14 01:06:12 +0530

    connected to db ,npm i mongoose,express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("This is backend code")
+const express= require('express')
+const mongoose = require('mongoose')
+
+const app= express()
+mongoose.connect("mongodb+srv://admin:5hFg7rXsULjnAh6a@cluster0.ni6vsgm.mongodb.net/bookStore?retryWrites=true&w=majority").then(()=>{
+  console.log("connected to db")
+}).then(()=>{
+    app.listen(5000)
+}).catch((err)=>{
+    console.log(err.message)
+})
