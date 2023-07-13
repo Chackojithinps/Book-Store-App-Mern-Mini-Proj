3b552336fa3dd4fae5e0a1ada0e06b12d290a5b7
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-14 01:25:27 +0530

    model created for book

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express= require('express')
 const mongoose = require('mongoose')
 
 const app= express()
+
+//Middlewares
+
+app.use('/',(req,res,next)=>{
+  res.send("This is our starting application")
+})
+
 mongoose.connect("mongodb+srv://admin:5hFg7rXsULjnAh6a@cluster0.ni6vsgm.mongodb.net/bookStore?retryWrites=true&w=majority").then(()=>{
   console.log("connected to db")
 }).then(()=>{
