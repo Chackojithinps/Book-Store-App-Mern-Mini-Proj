04c1973140fe00ce4b6e988f50835c47d1ad5feb
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-14 14:04:06 +0530

    header styling

--- book-store/src/Components/Header.js
+++ book-store/src/Components/Header.js
@@ -1,14 +1,19 @@
 import {React} from 'react'
-import {AppBar, Toolbar, Typography} from '@mui/material'
-import MenuBookIcon from '@mui/icons-material/MenuBook';
+import {AppBar, Tab, Tabs, Toolbar, Typography} from '@mui/material'
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 const Header =() =>{
    return (
     <div>
        <AppBar position='sticky'>
          <Toolbar>
            <Typography>
-              <MenuBookIcon/>
+              <LibraryBooksIcon/>
            </Typography>
+
+              <Tabs textColor='inherit' indicatorColor='secondary' value={0}>
+                 <Tab label="Add product" />
+                 <Tab label ="About us" />
+              </Tabs>
            </Toolbar>
        </AppBar>
     </div>
